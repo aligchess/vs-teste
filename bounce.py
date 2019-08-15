2026816641f429ea7f1324ba24e64fcd99f98401
Cleilton Carvalho <cleiltoncs@hotmail.com>
2019-08-15 09:17:49 -0300

    Main screen

--- bounce.py
+++ bounce.py
@@ -1,10 +1,16 @@
-import math
-import os
+"""Creating a bounce game"""
+from tkinter import *
+import time
+import random
 
-import requests
+tk = Tk()
+tk.title("Game")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
 
-print("Gol!")
-
-name = input("Digite seu nome: ")
-idade = int(input("digite sua idade: "))
-print("hello, ", name)
+while 1:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)

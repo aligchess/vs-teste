9a056a77032a0fbb128f975a0c85d020b816e999
Cleilton Carvalho <cleiltoncs@hotmail.com>
2019-08-26 23:52:18 -0300

    Making the Ball Bounce

--- bounce.py
+++ bounce.py
@@ -9,9 +9,19 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
+        self.x = 0
+        self.y = -1
+        self.canvas_height = (
+            self.canvas.winfo_height()
+        )  # Return the current height of the canvas
 
     def draw(self):
-        self.canvas.move(self.id, 0, -1)
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
 
 
 tk = Tk()
@@ -20,6 +30,7 @@ tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
+tk.update()
 ball = Ball(canvas, "red")
 
 while 1:
